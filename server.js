841e4998c53c7c8e483c6237dda6cd6ba3d6e54c
noel paganelli <noel.paganelli@free.fr>
2017-03-29 14:29:07 +0200

    Add API TRELLO

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var request = require('request');
 var mongoose= require('mongoose');
 var session = require("express-session");
+var Trello  = require("node-trello");
+ 
 mongoose.connect('mongodb://localhost/mymoviesapp' , function(err) {
 
 });
@@ -20,14 +22,19 @@ var userSchema = mongoose.Schema({
   },
   { collection: 'userslist' }
 );
-
 var UserModel = mongoose.model('userslist', userSchema);
+
+
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-
-
+app.use(
+ session({ 
+  secret: 'a4f8071f-c873-4447-8ee2',
+  resave: false,
+  saveUninitialized: false,
+ })
+);
 
 app.get('/', function (req, res) {
   request("https://api.themoviedb.org/3/discover/movie?api_key=1ca44169216245030924859d77648835&language=fr-FR&region=fr&sort_by=popularity.desc&include_adult=false&include_video=false&page=1", function(error, response, body) {
@@ -42,7 +49,7 @@ app.get('/', function (req, res) {
 });
 
 
-var isLog = false;
+//var isLog = false;
 
 app.get('/review', function (req, res) {
   console.log(req.query.email+' // '+req.query.motdepasse);
@@ -53,12 +60,13 @@ app.get('/review', function (req, res) {
       console.log(userList[i].login+' / '+userList[i].password);
       if(req.query.email == userList[i].login) {
         if(req.query.motdepasse == userList[i].password) {
-          isLog = true;
+          //isLog = true;
+          req.session.isLog = true;
         }
       }
     }
     
-    if(isLog == true) {
+    if(req.session.isLog == true) {
       MovieModel.find(function (err, movies) {
         res.render('review', {moviesList : movies});
       })
@@ -68,14 +76,13 @@ app.get('/review', function (req, res) {
   
    });
     
-  /*
   if(req.query.email == "noel@lacapsule.academy") {
      console.log("email OK");
      if(req.query.motdepasse == "riri") {
         console.log("email et mot de passe OK");
         isLog = true;
     }
-  }*/
+  }
 
  
 
@@ -84,6 +91,16 @@ app.get('/review', function (req, res) {
 app.get('/contact', function (req, res) {
   res.render('contact');
 });
+
+app.get('/addcontact' , function(req, res) {
+  
+  var t = new Trello("568526dad1bd8f7874fadea91a432cdd", "29d125a99ba731091e55d920686c23dd41cf1f5c9ed272d70ba306e6130bb17f");
+  t.post("/1/cards",  { idList : "57d278fee99551079c5ce0f0", name: req.query.lastName+' / '+req.query.email }, function(err, data) {
+    res.render('contactconfirm');
+  });
+
+})
+
 
 app.get('/single', function (req, res) {
   request("https://api.themoviedb.org/3/movie/"+req.query.id+"?api_key=1ca44169216245030924859d77648835&language=fr-FR", function(error, response, body) {
